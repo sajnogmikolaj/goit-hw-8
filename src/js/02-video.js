8900a206f6ea6e33831ebf7d8281230d0a7f115e
sajnogmikolaj <130835999+sajnogmikolaj@users.noreply.github.com>
2024-04-20 12:44:14 +0200

    skibidi

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const player = new Player(iframe);
 
+const iframe = document.getElementById('vimeo-player');
+const VIDEO_STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(throttled(data), 1000));
+
+function throttled(data) {
+    //   console.log('Percentage watched: ' + data.seconds);
+    localStorage.setItem(`${VIDEO_STORAGE_KEY}`, data.seconds);
+}
+player.setCurrentTime(localStorage.getItem("VIDEO_STORAGE_KEY"))
+
+throttled(data)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+// const dataSend = () => {
+//     const timerId = setTimeout(() => {
+//         let textValue = document.getElementById('text').value;
+//         let emailValue = document.getElementById('email').value;
+        
+//     }, 5000);
+//     localStorage.setItem(emailValue, textValue)
+// }
 
+
+// // const btn = document.getElementById('button')
+
+// // btn.addEventListener('click', dataSend);
+
+
+const form = document.querySelector('form.feedback-form');
+const email = form.elements.email;
+const message = form.elements.message;
