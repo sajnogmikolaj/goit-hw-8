2a9637cdd0e5a14b5b1ea7c05b04eeefe18e9250
sajnogmikolaj <130835999+sajnogmikolaj@users.noreply.github.com>
2024-04-22 18:53:45 +0200

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,13 @@
 import Player from '@vimeo/player';
-const player = new Player(iframe);
-
+import throttle from 'lodash.throttle';
 const iframe = document.getElementById('vimeo-player');
 const VIDEO_STORAGE_KEY = 'videoplayer-current-time';
-
-player.on('timeupdate', throttle(throttled(data), 1000));
-
-function throttled(data) {
-    //   console.log('Percentage watched: ' + data.seconds);
-    localStorage.setItem(`${VIDEO_STORAGE_KEY}`, data.seconds);
-}
-player.setCurrentTime(localStorage.getItem("VIDEO_STORAGE_KEY"))
-
-throttled(data)
+const player = new Player(iframe);
+if (localStorage.key(VIDEO_STORAGE_KEY))
+  player.setCurrentTime(localStorage.getItem(VIDEO_STORAGE_KEY));
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(VIDEO_STORAGE_KEY, data.seconds);
+  }, 1000)
+);
