7cd1a5b77a3a588e2ac6f907e9f238ea90995502
sajnogmikolaj <130835999+sajnogmikolaj@users.noreply.github.com>
2024-04-22 17:52:43 +0000

    Deploying to gh-pages from @ sajnogmikolaj/goit-hw-8@644b353bb41ff9adc9d861b05ad316c2c36164fd 🚀

--- 03-feedback.20b08b44.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form.feedback-form");e.elements.email,e.elements.message;
-//# sourceMappingURL=03-feedback.20b08b44.js.map
--- 03-feedback.2cf7daa7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("form.feedback-form");e.elements.email,e.elements.message}();
-//# sourceMappingURL=03-feedback.2cf7daa7.js.map
